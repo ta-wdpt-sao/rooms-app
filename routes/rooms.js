622870c8cf3ada0f04447dde504bd3a48b185db3
guilhermeaeme <guilhermeaeme@gmail.com>
2019-02-23 15:34:00 -0300

    rooms read update & delete

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router  = express.Router();
 const Room = require('../models/room');
+const Review = require('../models/review');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('rooms/index');
+  Room.find({})
+    .then(rooms => {
+      res.render('rooms/index', { rooms });
+    })
+    .catch(error => {
+      throw new Error(error);
+    });
 });
 
 router.get('/add', (req, res, next) => {
@@ -56,6 +63,93 @@ router.post('/add', (req, res, next) => {
     .catch(err => { throw new Error(err)});
   })
   .catch(err => { throw new Error(err)});
+});
+
+router.get('/edit/:id', (req, res, next) => {
+  const roomId = req.params.id;
+
+  Room.findOne({ _id: roomId })
+    .then(room => {
+      res.render('rooms/form', { room } );
+    })
+    .catch(error => {
+      throw new Error(error);
+    });
+});
+
+router.post("/edit", (req, res, next) => {
+  const roomId = req.body._id;
+
+  const {
+    name,
+    description,
+    imageUrl
+  } = req.body;
+
+  const location = {
+    type: 'Point',
+    coordinates: [req.body.longitude, req.body.latitude]
+  };
+
+  Room.update(
+    { _id: roomId },
+    { $set: {
+      name,
+      description,
+      imageUrl
+     }
+    },
+    { new: true } 
+  )
+    .then(room => {
+      res.redirect('/rooms');
+    })
+    .catch(error => {
+      throw new Error(error);
+    });
+});
+
+router.get('/delete/:id', (req, res, next) => {
+  let roomId = req.params.id;
+
+  if (!/^[0-9a-fA-F]{24}$/.test(roomId)) return res.status(404).send('not-found');
+
+  Room.findOne({ _id: roomId })
+    .then(room => {
+      res.render('rooms/delete', { room });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+router.post('/delete', (req, res, next) => {
+  let roomId = req.body._id;
+
+  if (!/^[0-9a-fA-F]{24}$/.test(roomId)) return res.status(404).send('not-found');
+
+  Room.deleteOne({ _id: roomId })
+    .then(room => {
+      res.render("rooms/delete", { message: `room ${room.name} deleted!` });    
+    })
+    .catch(err => {
+      throw new Error(err);
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+  let roomId = req.params.id;
+
+  if (!/^[0-9a-fA-F]{24}$/.test(roomId)) return res.status(404).send('not-found');
+
+  Room.findOne({ _id: roomId })
+    .populate('reviews')
+    .then(room => {
+      res.render('rooms/detail', { room });
+    })
+    .catch(error => {
+      throw new Error(error);
+    });
 });
 
 module.exports = router;
