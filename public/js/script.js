dd0eb26bae693582e1bda6a18dc01da7dbaa04f1
guilhermeaeme <guilhermeaeme@gmail.com>
2019-02-28 20:44:09 -0300

    gmaps & geocoder

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,23 @@
-console.log('JS is running?')
+$(function(){
+  if($('#address').length > 0){
+    const geocoder = new google.maps.Geocoder();
+
+    $('#address').on('blur', function () {
+      geocodeAddress(geocoder);
+    });
+
+    function geocodeAddress(geocoder, resultsMap) {
+      let address = $('#address').val();
+    
+      geocoder.geocode({ 'address': address }, function (results, status) {
+    
+        if (status === 'OK') {
+          $('#latitude').val(results[0].geometry.location.lat());
+          $('#longitude').val(results[0].geometry.location.lng());
+        } else {
+          alert('Geocode was not successful for the following reason: ' + status);
+        }
+      });
+    }
+  }
+});
